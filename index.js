e5105533ee631d3f4a2d4841ba43342c82c7f0af
rafia-khan <khanrafia9218@gmail.com>
2025-02-21 01:55:18 +0600

    css

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function increament(incdec,prc,itm) {
     var count = document.getElementById(incdec);
     var price = document.getElementById(prc);
     var item = document.getElementById(itm);
-    var pro_amount= document.getElementById("pro_amount")
+    var pro_amount = document.getElementById("pro_amount");
+    var charge = document.getElementById("charge");
+    var total_amount = document.getElementById("total_amount")
 
 
     if (count.value >= 5) {
@@ -16,8 +18,11 @@ function increament(incdec,prc,itm) {
         var result = parseInt(price.innerHTML) + parseInt(item.innerHTML)
         item.innerHTML = result
         
-        var total = parseInt(price.innerHTML) + parseInt(pro_amount.innerHTML)
-        pro_amount.innerHTML = total
+        var total_product = parseInt(price.innerHTML) + parseInt(pro_amount.innerHTML)
+        pro_amount.innerHTML = total_product
+
+        var NewResult = total_product + parseInt(charge.innerHTML)
+        total_amount.innerHTML = NewResult
     }
 }
 
@@ -25,7 +30,9 @@ function decreament(incdec,prc,itm) {
     var count = document.getElementById(incdec);
     var price = document.getElementById(prc);
     var item = document.getElementById(itm);
-    var pro_amount= document.getElementById("pro_amount")
+    var pro_amount = document.getElementById("pro_amount");
+     var charge = document.getElementById("charge");
+    var total_amount = document.getElementById("total_amount")
 
     if (count.value <= 0) {
         count.value = 0;
@@ -37,8 +44,13 @@ function decreament(incdec,prc,itm) {
         var result = parseInt(item.innerHTML) - parseInt(price.innerHTML)
         item.innerHTML = result
         
-        var total = parseInt(pro_amount.innerHTML) - parseInt(price.innerHTML)
-        pro_amount.innerHTML = total
+        var total_product = parseInt(pro_amount.innerHTML) - parseInt(price.innerHTML)
+        pro_amount.innerHTML = total_product
+
+        var NewResult = total_product + parseInt(charge.innerHTML)
+        total_amount.innerHTML = NewResult
+
+        
     }
 
 
