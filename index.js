3553283b12384c62ea710c78edfdb1e8ac8932ec
rafia-khan <khanrafia9218@gmail.com>
2025-02-20 21:56:50 +0600

    tuhin

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function increament() {
-    var count = document.getElementById("count");
+function increament(incdec,prc,itm) {
+    var count = document.getElementById(incdec);
+    var price = document.getElementById(prc);
+    var item = document.getElementById(itm);
+    var pro_amount= document.getElementById("pro_amount")
+
 
     if (count.value >= 5) {
         count.value = 5;
@@ -8,11 +12,20 @@ function increament() {
 
     else {
         count.value++;
+
+        var result = parseInt(price.innerHTML) + parseInt(item.innerHTML)
+        item.innerHTML = result
+        
+        var total = parseInt(price.innerHTML) + parseInt(pro_amount.innerHTML)
+        pro_amount.innerHTML = total
     }
 }
 
-function decreament() {
-    var count = document.getElementById("count");
+function decreament(incdec,prc,itm) {
+    var count = document.getElementById(incdec);
+    var price = document.getElementById(prc);
+    var item = document.getElementById(itm);
+    var pro_amount= document.getElementById("pro_amount")
 
     if (count.value <= 0) {
         count.value = 0;
@@ -21,6 +34,11 @@ function decreament() {
 
     else {
         count.value--;
+        var result = parseInt(item.innerHTML) - parseInt(price.innerHTML)
+        item.innerHTML = result
+        
+        var total = parseInt(pro_amount.innerHTML) - parseInt(price.innerHTML)
+        pro_amount.innerHTML = total
     }
 
 
